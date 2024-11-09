3e85e230cdbbbf7ba151aff29447fcd539223d7d
Alan <alanquispe022@gmail.com>
2024-11-09 10:16:01 -0400

    Implementando la funcionalidad de Cargando... en boton, opcion 1

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,13 @@ function App() {
         </h1>
         <div className="row">
           <div className="col-1">
-            <Button />
-          </div>
-          <div className="col-1">
-            <Button />
-          </div>
-          <div className="col-1">
             <Card>
               <BodyCard
                 title="Card title"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
               {elementos}
+              <Button text="Enviar" />
             </Card>
           </div>
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,31 @@
-function Button() {
-  return <button className="btn btn-success">Soy un boton</button>;
+import { useState } from "react";
+
+type buttonProps = {
+  text: string;
+};
+
+function Button({ text }: buttonProps) {
+  function onClick() {
+    setLoading(!loading);
+    console.log("Imprimiendo");
+  }
+  const [loading, setLoading] = useState(false);
+
+  let classBtn = "primary";
+  let content = text;
+  if (loading) {
+    classBtn = "secondary";
+    content = "Cargando...";
+  }
+  return (
+    <button
+      onClick={() => onClick()}
+      className={`btn btn-${classBtn} mt-2`}
+      disabled={loading}
+    >
+      {content}
+    </button>
+  );
 }
 
 export default Button;
